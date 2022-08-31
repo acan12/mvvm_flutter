ea52ebb7624c6a926fb211ddce8877f4f506a011
ary <ary@clappingape.com>
2022-08-31 12:09:47 +0700

    add sample reactive programming in flutter using riverpod StateProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,31 @@
-import 'package:dio/dio.dart';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'data/client.dart';
-import 'data/remote/response/user_response.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
-}
+final numberProvider = StateProvider<int>((ref) => 0);
 
-class MyApp extends StatelessWidget {
-// to set the root of app.
+void main() => runApp(ProviderScope(child: MyApp()));
+
+class MyApp extends ConsumerWidget {
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Sample")),
+        body: Center(
+          child: Text(
+            ref.watch(numberProvider).toString())
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            ref.read(numberProvider.notifier).state++;
+          },
+        ),
       ),
-      home: MyHomePage(title: 'API Demo Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Flutter - Retrofit"),
-      ),
-      body: _buildBody(context),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Icon(Icons.cancel),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
-
-  // build list view & manage states
-  FutureBuilder<UserResponse> _buildBody(BuildContext context) {
-    final client = APIClient(Dio(BaseOptions(contentType: "application/json")));
-    return FutureBuilder<UserResponse>(
-      future: client.getPersona(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          final UserResponse? persona = snapshot.data;
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(child: Text(persona?.full_name ?? "")),
-              Center(
-                  child: Text(persona?.email ?? "",
-                      style: TextStyle(fontWeight: FontWeight.bold)))
-            ],
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
     );
   }
 }
---
+++ lib/ui/provider/provider_page.dart
@@ -0,0 +1,18 @@
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/';
+// class ProviderPage extends StatelessWidget {
+//
+//   final numberProvider = Provider<int>((ref)) => 42);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Provider"),,
+//       ),
+//       body: Container(),
+//     )
+//   }
+// }
