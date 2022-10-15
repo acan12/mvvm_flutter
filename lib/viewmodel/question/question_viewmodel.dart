f66e4ff0046ad8de0a3a80d634484b1fadf4a2d6
ary <ary@clappingape.com>
2022-10-15 22:33:35 +0700

    added sample for unit test

--- lib/viewmodel/question/question_viewmodel.dart
+++ lib/viewmodel/question/question_viewmodel.dart
@@ -5,30 +5,35 @@ import '../../data/model/entity/employee.dart';
 import '../../data/model/response/question/question_detail_response.dart';
 import '../../repo/question/question_repository.dart';
 import '../../repo/user/user_repository.dart';
+import '../question_service.dart';
 
-class QuestionPageViewModel extends BaseViewModel {
+class QuestionPageViewModel extends BaseViewModel implements QuestionService {
   final QuestionRepository questionRepo = QuestionRepository();
   final UserRepository userRepo = UserRepository();
 
   var listEmployee = <Employee>[];
 
+  @override
   Future<QuestionResponse> getQuestions() => questionRepo.getQuestions();
 
+  @override
   Future<QuestionDetailResponse> getDetailQuestion() =>
       questionRepo.getDetailQuestion();
 
-  void getAllEmployee() async{
+  @override
+  void getAllEmployee() async {
     listEmployee = await userRepo.getAllEmployee();
-    // final result = await userRepo.getAllEmployee();
     notifyListeners();
   }
 
+  @override
   void addEmployee(Employee employee) {
     userRepo.addEmployment(employee);
     listEmployee.add(employee);
     notifyListeners();
   }
 
+  @override
   void deleteAllEmployee() {
     userRepo.deleteAllEmploye();
     listEmployee.clear();
--- test/main_page_test.dart
+++ test/main_page_test.dart
@@ -1,7 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:freezed_demo/viewmodel/question/question_viewmodel.dart';
+import 'package:freezed_demo/viewmodel/question_service.dart';
+import 'package:mocktail/mocktail.dart';
 
-void main(){
-  testWidgets('Test Widget', (widgetTester) async {
+class MockQuestionService extends Mock implements QuestionService {}
 
+void main() {
+  late QuestionPageViewModel viewmodel;
+  late MockQuestionService service;
+
+  setUp(() {
+    viewmodel = QuestionPageViewModel();
+    service = MockQuestionService();
   });
+
+  test(
+    "initial values are correct",
+    () {
+      expect(viewmodel.listEmployee, []);
+    },
+  );
+
+  group('get questions', () {
+    test('get questions from api', () async {
+      // when(() => service.getQuestions()).thenAnswer((_) async => []);
+      await viewmodel.getQuestions();
+      verify(() => service.getQuestions()).called(1);
+    });
+  });
+
+  // testWidgets('Test Widget', (widgetTester) async {});
 }
