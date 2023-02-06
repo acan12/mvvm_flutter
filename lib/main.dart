d1defd59ff63e75abd85a4be209e8406388b40ac
ary <ary@clappingape.com>
2023-02-06 15:20:21 +0700

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_demo/data/model/entity/employee.dart';
 import 'package:freezed_demo/routes.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,10 +10,9 @@ void main() async {
   // await path_provider.getApplicationDocumentsDirectory();
   // GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   await Hive.initFlutter();
-  Hive
-    ..registerAdapter(EmployeeAdapter());
-    // ..registerAdapter(JobAdapter())
-    // ..registerAdapter(DepartmentAdapter());
+  Hive..registerAdapter(EmployeeAdapter());
+  // ..registerAdapter(JobAdapter())
+  // ..registerAdapter(DepartmentAdapter());
 
   runApp(MyApp());
 }
@@ -27,15 +24,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
         child: MaterialApp.router(
-          routerDelegate: goRouter.routerDelegate,
-          routeInformationParser: goRouter.routeInformationParser,
-          routeInformationProvider: goRouter.routeInformationProvider,
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-          ),
-        ));
+      routerDelegate: goRouter.routerDelegate,
+      routeInformationParser: goRouter.routeInformationParser,
+      routeInformationProvider: goRouter.routeInformationProvider,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+    ));
   }
 }
-
-
